7d776a0583ba86b50c7e8d353ec74079a8a013cf
Rafael Cerqueira <up201910200@up.pt>
2024-04-17 11:29:27 +0100

    early code for basic connection between game and client

--- README.md
+++ README.md
@@ -7,6 +7,12 @@ Group members:
 2. Rafael Cerqueira(up201910200@up.pt)
 3. Gaspar Faria(up202108797@up.pt)
 
+# Run Code
+- To run the code you need to compile the Game.java (wich is the Server) and compile the Client.java 
+- Then you need to use:
+    - `java Game 8000` for the Server side
+    - `java Client localhost 8000` for the Client side
+
 # Game Description
 The game implemented has the following logic:
 - The server chooses a number from 1 to 100
---
+++ assign2/src/Game.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.net.*;
+import java.util.Date;
+ 
+public class Game {
+ 
+    public static void main(String[] args) {
+        if (args.length < 1) return;
+ 
+        int port = Integer.parseInt(args[0]);
+ 
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+ 
+            System.out.println("Server is listening on port " + port);
+ 
+            while (true) {
+                Socket socket = serverSocket.accept();
+ 
+                InputStream input = socket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+ 
+                String time = reader.readLine();
+
+                System.out.println("New client connected: "+ time);
+ 
+                OutputStream output = socket.getOutputStream();
+                PrintWriter writer = new PrintWriter(output, true);
+ 
+                writer.println(new Date().toString());
+            }
+ 
+        } catch (IOException ex) {
+            System.out.println("Server exception: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+}
